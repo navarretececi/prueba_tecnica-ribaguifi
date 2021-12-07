9d2062098817682b33985e247e730c1e3defe0f2
Cecilia Navarrete <navarretececi@gmail.com>
2021-12-07 20:50:57 -0300

    add var white

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
-    path('2', TemplateView.as_view(template_name='index2.html'))
+    path('page-2', TemplateView.as_view(template_name='page2.html')),
+    path('page-3', TemplateView.as_view(template_name='page3.html')),
 ]
