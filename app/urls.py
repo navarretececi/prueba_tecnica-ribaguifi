af0800d603b4d3c621c8cee8f1a6b46f31401a21
Cecilia Navarrete <navarretececi@gmail.com>
2021-12-08 18:51:17 -0300

    delete base.html and add zigzag arrow

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
-    path('page-2', TemplateView.as_view(template_name='page2.html')),
-    path('page-3', TemplateView.as_view(template_name='page3.html')),
+    path('page-2', TemplateView.as_view(template_name='page2.html'))
 ]
