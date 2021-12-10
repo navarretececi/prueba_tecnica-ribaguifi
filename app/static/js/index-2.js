5e484f2a106c90dab3b9920967bb10a2c4349584
Cecilia Navarrete <navarretececi@gmail.com>
2021-12-10 17:33:24 -0300

    add library swiper for horizontal selector

---
+++ app/static/js/index-2.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 3,
+  center:true,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
